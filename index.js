3e3feb17f8084fa2fdd0bcdb8601c0d97894ca57
Richie Bendall <richiebendall@gmail.com>
2020-01-01 22:24:10 +1300

    feat: Add linux and cygwin support
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 const platform = require("os").platform()
 
-module.exports = () => {
+module.exports = (() => {
     if (platform === "win32") {
-        return require("./lib/windows")()
+        return require("./lib/windows")
     } else if (platform === "darwin") {
-        return require("./lib/macos")()
+        return require("./lib/macos")
+    } else if (platform === "linux" || platform === "cygwin") {
+        return require("./lib/linux")
+    } else {
+        throw new Error(`${platform} is not supported!`)
     }
-}
+})()
