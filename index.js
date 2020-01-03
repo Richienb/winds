f6f88578bd957bd79791ba04d4d13593c6b05b8e
Richie Bendall <richiebendall@gmail.com>
2020-01-04 03:38:16 +1300

    fix: Proper placement of checks
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const platform = require("os").platform()
 
 module.exports = (() => {
-    if (platform === "win32") return require("./lib/windows" || platform === "cygwin")
+    if (platform === "win32" || platform === "cygwin") return require("./lib/windows")
     if (platform === "darwin") return require("./lib/macos")
     if (platform === "linux") return require("./lib/linux")
     throw new Error(`${platform} is not supported!`)
